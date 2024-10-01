ca71b245472f49cde4f77f2c559fbc790a741532
yasminguimaraes098 <162388923+yasminguimaraes098@users.noreply.github.com>
2024-09-30 21:37:49 -0300

    Add files via upload

---
+++ lista01/ex10/javascript/script.js
@@ -0,0 +1,7 @@
+var numero = parseInt(prompt("Digite um número inteiro positivo que seja maior que 0"));
+
+var nome = prompt("Digite seu nome completo");
+
+for (var i = 0; i < numero; i++) {
+    document.write(`${nome}</br>`);
+}
